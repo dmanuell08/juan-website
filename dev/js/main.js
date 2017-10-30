35dd1041edaccb457b3973f0d018c2621fdaf965
Kevin Toro <kctoro74@gmail.com>
2017-10-30 18:28:32 -0500

    Implementación de scrollspy

--- dev/js/main.js
+++ dev/js/main.js
@@ -20,40 +20,19 @@ $(document).ready(function(){
             $("#fixed-nav").addClass("fixed-nav");
             $(".logo-juan").css({"display": "none"});
             $(".Logo_black").css({"display": "inline-block"});
-            $("#Home_section").removeClass("active");
         }
         else if(posicionDeWindow < 520){
             $("#fixed-nav").removeClass("fixed-nav");
             $(".logo-juan").css({"display": "initial"});
             $(".Logo_black").css({"display": "none"});
-            $("#Home_section").addClass("active");
         }
 
         if(posicionDeWindow > 600){
             $(".fixed-nav").addClass("nav_visible");
-            $("#Work").addClass("active");
         }
 
         else if(posicionDeWindow < 600){
-            $("#Work").removeClass("active");
             $(".fixed-nav").removeClass("nav_visible");
-        } 
-
-        if(posicionDeWindow > 2550){
-            $("#Work").removeClass("active");
-            $("#What_i_do").addClass("active");
-        }
-
-        else if(posicionDeWindow < 2550){
-            $("#What_i_do").removeClass("active");
-        } 
-
-        if(posicionDeWindow > 3900){
-        	$("#Contact_me").addClass("active");
-        	$("#What_i_do").removeClass("active");
-        } 
-        else if(posicionDeWindow < 3900){
-        	$("#Contact_me").removeClass("active");
-        }           
+        }         
     });
 });
